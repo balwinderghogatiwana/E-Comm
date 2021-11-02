5ff8f20aa7599b5207b35a1cc106083e9f2e0739
Ghoga <balwinderghoga8@gmail.com>
2021-11-02 22:27:51 +0530

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,20 +4,21 @@
 <div class="container custom-login">
     <div class="row d-flex justify-content-center">
         <div class="col-sm-4 col-sm-offset-4">
-            <form>
+            <form action="login" method="POST">
                 <div class="row mb-3">
+                  @csrf
                     <label for="exampleFormControlInput1" class="col-form-label">Email address</label>                 
                     <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Enter email">
+                    <input type="email" name="email" class="form-control" id="inputEmail3" placeholder="Enter email">
                   </div>
                 </div>
                 <div class="row mb-3">
                   <label for="inputPassword3" class="col-form-label">Password</label>
                   <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Enter password">
+                    <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Enter password">
                   </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
+                <button type="submit" class="btn btn-primary">Login</button>
               </form>
         </div>
     </div>
