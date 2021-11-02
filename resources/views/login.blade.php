ccf6262dd45a4e582d4e44a9286215f41d9a5453
Vikram_Singh <balwinderghoga8@gmail.com>
2021-11-02 14:28:40 +0530

    login file modifiy

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,14 +6,15 @@
         <div class="col-sm-4 col-sm-offset-4">
             <form>
                 <div class="row mb-3">
-                    <label for="exampleFormControlInput1" class="col-form-label">Email address</label>                  <div class="col-sm-10">
+                    <label for="exampleFormControlInput1" class="col-form-label">Email address</label>                 
+                    <div class="col-sm-10">
                     <input type="email" class="form-control" id="inputEmail3" placeholder="Enter email">
                   </div>
                 </div>
                 <div class="row mb-3">
                   <label for="inputPassword3" class="col-form-label">Password</label>
                   <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Enter Password">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Enter password">
                   </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Sign in</button>
